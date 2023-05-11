70d688aaffb92d9d68091c488110c974fed2e6bb
Andreas Bücking <ab@seitenarchitekt.de>
2023-05-11 23:19:03 +0200

    Rector fixes

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['avatarProviders']['gravatarProvider'] = [
     'provider' => \MiniFranske\Gravatar\AvatarProvider\GravatarProvider::class,
